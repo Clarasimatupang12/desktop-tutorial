8dcdcc90231bc9236c93f6e6c3c58e10e7b53647
Clara Feronica Simatupang <clarasimatupang0@gmail.com>
2025-05-17 22:39:13 +0700

    Add files via upload

---
+++ pertemuan7soal1.js
@@ -0,0 +1,11 @@
+// Problem 1 - String Concatenation
+let firstName = 'clara';
+let LastName = 'simatupang';
+let bornYear = 2006;
+
+// Calculate age based on the current year (2023)
+let currentYear = 2025;
+let age = currentYear - bornYear;
+
+// Display the personalized message
+console.log(`Hello, ${firstName} ${LastName}! You are ${age} years old.`);
